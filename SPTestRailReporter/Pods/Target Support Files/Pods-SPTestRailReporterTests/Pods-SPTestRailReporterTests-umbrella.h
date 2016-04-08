229d82885987ac16e6f80a31ea74c202f1b5d0ab
Siddartha Polisetty <siddarthpolishetty@yahoo.com>
2016-04-08 10:46:43 -0700

    use frameworks in pods

---
+++ SPTestRailReporter/Pods/Target Support Files/Pods-SPTestRailReporterTests/Pods-SPTestRailReporterTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SPTestRailReporterTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SPTestRailReporterTestsVersionString[];
+

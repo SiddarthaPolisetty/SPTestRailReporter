20cb5d0ed86d91cb85b8c589157860a808cc5d17
Siddartha Polisetty <siddarthpolishetty@yahoo.com>
2016-04-11 10:00:33 -0700

    SPTR build scripts fix

--- cleanbuildtest.sh
+++ cleanbuildtest.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd SPTestRailReporterExample
+pod install
+cd ..
 xcodebuild -workspace SPTestRailReporterExample/SPTestRailReporterExample.xcworkspace -scheme SPTestRailReporterExample -sdk iphonesimulator9.3 clean
 xcodebuild -workspace SPTestRailReporterExample/SPTestRailReporterExample.xcworkspace -scheme SPTestRailReporterExample -sdk iphonesimulator9.3
 xcodebuild -workspace SPTestRailReporterExample/SPTestRailReporterExample.xcworkspace -scheme SPTestRailReporterExample -sdk iphonesimulator9.3 test

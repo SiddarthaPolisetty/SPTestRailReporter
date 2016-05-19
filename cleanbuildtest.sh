1524cb4a751d2d4218f6f99d310547ae888303bb
Siddartha Polisetty <siddarthpolishetty@yahoo.com>
2016-05-19 11:15:15 -0700

    Updated build scripts

--- cleanbuildtest.sh
+++ cleanbuildtest.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 cd Example
-pod repo remove master
-pod setup
-pod clean
 pod install
 xcodebuild -workspace Example.xcworkspace -scheme Example -sdk iphonesimulator9.3 clean
 xcodebuild -workspace Example.xcworkspace -scheme Example -sdk iphonesimulator9.3
